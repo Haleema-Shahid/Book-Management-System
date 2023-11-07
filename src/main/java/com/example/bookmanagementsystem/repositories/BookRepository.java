72b0dadb134c908747b00a714573e85d4f74b08e
Adil MIr <100213252+adilmir21@users.noreply.github.com>
2023-11-07 18:32:27 +0500

    Liquibase Implemented

--- src/main/java/com/example/bookmanagementsystem/repositories/BookRepository.java
+++ src/main/java/com/example/bookmanagementsystem/repositories/BookRepository.java
@@ -4,18 +4,11 @@ import com.example.bookmanagementsystem.entities.AuthorEntity;
 import com.example.bookmanagementsystem.entities.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-<<<<<<< HEAD
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<BookEntity, Integer> {
     Optional<BookEntity> findByTitle(String title);
-=======
-import java.util.List;
-
-@Repository
-public interface BookRepository extends JpaRepository<BookEntity, Integer> {
     List<BookEntity> findByAuthorByAuthorId(AuthorEntity authorByAuthorId);
->>>>>>> 68dbcb3b5c84fb9dbaee5d889a65324ccfbd2e90
 }
