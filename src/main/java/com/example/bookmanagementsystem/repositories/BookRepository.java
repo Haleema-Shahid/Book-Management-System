8a9999a28b927280d7c9bbd0af18f887ca9516db
MuhammadIsmaeelDev <136610882+MuhammadIsmaeelDev@users.noreply.github.com>
2023-11-07 18:23:17 +0500

    swagger added

--- src/main/java/com/example/bookmanagementsystem/repositories/BookRepository.java
+++ src/main/java/com/example/bookmanagementsystem/repositories/BookRepository.java
@@ -5,17 +5,12 @@ import com.example.bookmanagementsystem.entities.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-<<<<<<< HEAD
 import java.util.Optional;
 
-@Repository
-public interface BookRepository extends JpaRepository<BookEntity, Integer> {
-    Optional<BookEntity> findByTitle(String title);
-=======
 import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<BookEntity, Integer> {
+    Optional<BookEntity> findByTitle(String title);
     List<BookEntity> findByAuthorByAuthorId(AuthorEntity authorByAuthorId);
->>>>>>> 68dbcb3b5c84fb9dbaee5d889a65324ccfbd2e90
 }
