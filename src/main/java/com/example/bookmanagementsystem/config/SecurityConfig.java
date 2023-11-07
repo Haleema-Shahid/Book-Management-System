9252ca8f073a2031cd5d023c2d09f0136182c23d
haleema-shahid <haleemashahid@192.168.0.103>
2023-11-07 23:35:07 +0500

    changes

--- src/main/java/com/example/bookmanagementsystem/config/SecurityConfig.java
+++ src/main/java/com/example/bookmanagementsystem/config/SecurityConfig.java
@@ -1,16 +1,14 @@
 package com.example.bookmanagementsystem.config;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,6 +16,8 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 
 @Configuration
@@ -30,24 +30,27 @@ public class SecurityConfig {
 
 
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
-    }
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http,  HandlerMappingIntrospector introspector) throws Exception {
-        MvcRequestMatcher.Builder mvcRequestMatcher = new MvcRequestMatcher.Builder(introspector);
         //http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         //http.httpBasic(Customizer.withDefaults());
         //http.csrf(AbstractHttpConfigurer::disable);
         //http.cors(AbstractHttpConfigurer::disable);
-        http.headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));//headers().frameOptions().disable();
+        http.headers(httpSecurityHeadersConfigurer ->
+                httpSecurityHeadersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+
         http.authorizeHttpRequests(auth ->
         {
-            auth.requestMatchers(new AntPathRequestMatcher("/users/signup")).permitAll();
+            auth
+                    .requestMatchers(new AntPathRequestMatcher("/users/signup")).permitAll()
+                    //.requestMatchers(toH2Console()).permitAll()
+                    //.anyRequest().authenticated()
+
+            ;
             //auth.requestMatchers("/signup").permitAll();
-            auth.anyRequest().authenticated();
+            //auth.anyRequest().authenticated();
         });
 //                .requestMatchers("/swagger-ui-custom.html").permitAll()
 //                .requestMatchers("/swagger-ui/index.html").permitAll()
@@ -56,7 +59,7 @@ public class SecurityConfig {
 //        http.httpBasic(Customizer.withDefaults());
        // http.csrf(Customizer.withDefaults());
         http.cors(Customizer.withDefaults());
-        http.csrf(AbstractHttpConfigurer::disable);
+        http.csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.ignoringRequestMatchers(toH2Console()));
         http.addFilterBefore(jwtAuthFilter, BasicAuthenticationFilter.class);
         return http.build();
     }
